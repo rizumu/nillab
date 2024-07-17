e0dd4c56d1a6c44c46277a96dacdb414c9a72937
Thomas Schreiber <rizumu@users.noreply.github.com>
2024-07-17 01:20:38 -0400

    Add @nuxt/image.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   extends: 'content-wind',
+
   routeRules: {
     '/': { prerender: true }
-  }
+  },
+
+  compatibilityDate: '2024-07-17',
+  modules: ["@nuxt/image"]
 })
